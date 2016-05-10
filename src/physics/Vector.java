b4fe538e2473cef08f48e224207b86e83d48e412
Kartik Singh <17ksingh@westford-ma.gov>
2016-05-10 08:25:42 -0400

    Completed vector class

--- src/physics/Vector.java
+++ src/physics/Vector.java
@@ -1,9 +1,38 @@
 package physics;
 
 public class Vector {
-
-	public Vector() {
-		// TODO Auto-generated constructor stub
+	private double x, y;
+	
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public Vector add(Vector v) {
+		return new Vector(x + v.getX(), y + v.getY());
+	}
+	
+	public double magnitude() {
+		return Math.sqrt(x*x + y*y);
+	}
+	
+	public Vector unit() {
+		double mag = magnitude();
+		
+		return new Vector(x/mag, y/mag);
+	}
+	
+	public Vector scale(int mag) {
+		Vector unit = unit();
+		
+		return new Vector(mag*unit.getX(), mag*unit.getY());
+	}
 }
