0ef6d0e67d0f394cfed71613d87645bb4977d1b5
Kartik Singh <17ksingh@westford-ma.gov>
2016-05-23 10:15:06 -0400

    Added drawing for Mine

--- src/map/Mine.java
+++ src/map/Mine.java
@@ -1,19 +1,28 @@
 package map;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Point;
 
+import main.Window;
 import physics.CollisionHandler;
+import physics.Vector;
 
 public class Mine extends StaticMapItem implements Updatable {
-
-	public Mine() {
-		// TODO Auto-generated constructor stub
+	public static final Vector SIZE = new Vector(30, 30);
+	
+	public Mine(Vector position) {
+		super(position, SIZE);
 	}
 
 	@Override
 	public void draw(Graphics2D g2d) {
-		// TODO Auto-generated method stub
+		g2d.setColor(Color.MAGENTA);
 		
+		Point pos = Window.game2real(position);
+		Point sz = Window.game2real(size);
+		
+		g2d.fillRect((int) pos.getX(), (int) pos.getY(), (int) sz.getX(), (int) sz.getY());
 	}
 
 	@Override
