cb7a6721cf8f6e85c4bdd3ed208ce7394bead653
Kartik Singh <kartiksingh_ma@yahoo.com>
2016-06-03 17:36:36 -0500

    Fixed vertical text centering

--- src/main/FontHelper.java
+++ src/main/FontHelper.java
@@ -3,6 +3,9 @@ package main;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
 import java.io.File;
 
 public class FontHelper {
@@ -16,21 +19,26 @@ public class FontHelper {
 		return null;
 	}
 	
-	public static int stringWidth(String s, Font f, Graphics2D g2d) {
-		FontMetrics fm = g2d.getFontMetrics(f);
+	public static int stringWidth(String s, Graphics2D g2d) {
+		FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
 		
 		return fm.stringWidth(s);
 	}
 	
-	public static int centerStringX(String s, int w, Font f, Graphics2D g2d) {
-		FontMetrics fm = g2d.getFontMetrics(f);
+	public static int centerStringX(String s, int w, Graphics2D g2d) {
+		FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
 		
 		return (w - fm.stringWidth(s)) / 2;
 	}
 	
-	public static int centerStringY(int h, Font f, Graphics2D g2d) {
-		FontMetrics fm = g2d.getFontMetrics(f);
+	public static int centerStringY(String s, int h, Graphics2D g2d) {
+		FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
+		Rectangle r = fm.getStringBounds(s, g2d).getBounds();
 		
-		return (h - fm.getHeight()) / 2 + fm.getAscent();
+		FontRenderContext frc = g2d.getFontRenderContext();
+		GlyphVector gv = g2d.getFont().createGlyphVector(frc,  s);
+		Rectangle vb = gv.getVisualBounds().getBounds();
+		
+		return h/2 - vb.height/2 - vb.y;
 	}
 }
