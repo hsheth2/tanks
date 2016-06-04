6b158d01e978e206599b388d205130b1d89c988a
Kartik Singh <kartiksingh_ma@yahoo.com>
2016-06-04 17:52:50 -0400

    Halved grid size

--- src/main/Config.java
+++ src/main/Config.java
@@ -7,6 +7,6 @@ public class Config {
 	public static final int GAME_WIDTH = 6400;
 	public static final int GAME_HEIGHT = 4800;
 	
-	public static final int GRID_WIDTH = 40;
-	public static final int GRID_HEIGHT = 30;
+	public static final int GRID_WIDTH = 20;
+	public static final int GRID_HEIGHT = 15;
 }
