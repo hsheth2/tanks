ef9ffcf1ae4a5db1c716dba6eaf2f31f819a22e3
Harshal Sheth <hsheth2@gmail.com>
2016-06-13 22:09:52 -0400

    closing the files

--- src/main/AudioPlayer.java
+++ src/main/AudioPlayer.java
@@ -1,10 +1,14 @@
 package main;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineEvent.Type;
+import javax.sound.sampled.LineListener;
 
 public class AudioPlayer {
 	public static void play(String sound) {
@@ -17,6 +21,20 @@ public class AudioPlayer {
 
 			c.open(ais);
 			c.start();
+			c.addLineListener(new LineListener() {
+				@Override
+				public void update(LineEvent event) {
+					if (event.getType() == Type.STOP) {
+						try {
+							c.close();
+							ais.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.out.println("non-fatal error in sound system");
+						}
+					}
+				}
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
